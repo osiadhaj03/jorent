1aaf9804ca1c6f03b02968dc745531cc06f57668
aziz1132003 <mzyz247@gmail.com>
2025-05-10 21:07:30 +0300

    feat(routes): add resource route for contracts using ContractController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContractController;
 
 Route::get('/', function () {
     return redirect('/admin');
 });
+
+Route::resource('contracts', ContractController::class);
