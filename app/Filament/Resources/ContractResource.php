4ffaa1428eb3c7ad88405c17f06f3e6a3dd5e5df
osaidhaj <osaidhaj03@gmail.com>
2025-05-11 20:47:48 +0300

    refactor(ContractResource): remove form schema and related fields from the contract form

--- app/Filament/Resources/ContractResource.php
+++ app/Filament/Resources/ContractResource.php
@@ -29,66 +29,8 @@ class ContractResource extends Resource
     
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
 
-             Forms\Components\TextInput::make('tenant_national_id')
-                 ->required()
-                 ->label('الرقم الوطني للمستأجر')
-                 ->disabled(),
-             ///////
-
-      Forms\Components\Select::make('unit_id')
-          ->relationship('unit', 'unit_number')
-          ->required()
-          ->label('الوحدة')
-          ->reactive(),
-
-          
-
-
-              Forms\Components\DatePicker::make('start_date')
-                    ->required()
-                    ->label('تاريخ بداية العقد'),
-                Forms\Components\DatePicker::make('end_date')
-                    ->required()
-                    ->label('تاريخ نهاية العقد'),
-
-                Forms\Components\Select::make('payment_frequency')
-                    ->options([
-                        'monthly' => 'شهري',
-                        'quarterly' => 'ربع سنوي',
-                        'semi_annual' => 'نصف سنوي',
-                        'annual' => 'سنوي'
-                    ])
-                    ->required()
-                    ->label('تكرار الدفع'),
-                Forms\Components\TextInput::make('payment_amount')
-                    ->numeric()
-                    ->required()
-                    ->label('قيمة الدفعة'),
-                Forms\Components\Toggle::make('education_tax')
-                    ->default(true)
-                    ->label('ضريبة المعارف'),
-                Forms\Components\TextInput::make('education_tax_amount')
-                    ->numeric()
-                    ->label('قيمة ضريبة المعارف'),
-                Forms\Components\Textarea::make('property_fixtures')
-                //  ->required()
-                    ->label('محتويات العقار')
-                    ->disabled(),
-                Forms\Components\Textarea::make('additional_terms')
-                    ->label('شروط إضافية'),
-                Forms\Components\Select::make('status')
-                    ->options([
-                        'active' => 'فعال',
-                        'expired' => 'منتهي',
-                        'terminated' => 'ملغي'
-                    ])
-                    ->default('active')
-                    ->required()
-                    ->label('حالة العقد')
-            ]);
+    //
     }
 
     public static function table(Table $table): Table
